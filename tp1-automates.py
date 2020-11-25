2594074a4d4daba2e670eb4689781e1300f77c45
c18003304 <guillaume.coll@etu.univ-amu.fr>
2020-11-25 19:46:16 +0100

    c pas ouf

--- tp1-automates.py
+++ tp1-automates.py
@@ -17,5 +17,18 @@ if len(sys.argv) != 3:
 automatonfile = sys.argv[1]  
 word = sys.argv[2]
 
-print("TODO: faire le TP 1")  
-
+#qu'est ce que le chiffre dans statesdict ? 
+def isDetermine(automata):
+    for i in range(10):
+        if str(list(automata.statesdict["?"].transitions["a"])[i])>1:
+            return False
+        if str(list(automata.statesdict["?"].transitions["a"])[i])<=1:
+            return True
+        if str(list(automata.statesdict["?"].transitions["b"])[i])>1:
+            return False
+        if str(list(automata.statesdict["?"].transitions["b"])[i])<=1:
+            return True
+        
+        
+def isReconized(automata, wordtoreconize):
+    if 
